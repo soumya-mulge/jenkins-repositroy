5fcf4eb38260db5d14f5605b9a97bcd0a781c4b2
soumya <soumyam.rymec.ec15@gmail.com>
2020-07-31 20:03:29 +0530

    commiting program

---
+++ program.java
@@ -0,0 +1,8 @@
+class IntegerSumExample1 {  
+    public static void main(String[] args) {          
+      int a = 65;  
+        int b = 35;  
+        int sum=a+b;
+        System.out.println("The sum of a and b is = " + sum);  
+    }  
+}  
